59c666b3d77ce9fc63731a5b7a0d4243e078f967
kamioka24 <keee2404@gmail.com>
2021-09-29 12:05:02 +0900

    [Finish] Station4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React, { useState } from "react";
 import './App.css'
 
 /**
@@ -8,11 +8,12 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-blenheim/n02086646_1936.jpg");
   return (
     <div>
       <header>React_Station2</header>
       <p>犬の画像です</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-blenheim/n02086646_1936.jpg"/>
+      <img src={dogUrl} />
     </div>
   )
 }
