b4f6d2195ee0e460e4231b36c34fcee607578a5d
kamioka24 <keee2404@gmail.com>
2021-10-02 12:04:05 +0900

    [finish] station6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 // DO NOT DELETE
 
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import './App.css'
 
 /**
  *
  * @type {React.FC}
  */
+
 export const App = () => {
   const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-blenheim/n02086646_1936.jpg");
+
+  const updataImage = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then((data) => {
+      const status = data.status;
+      if (status === "success") {
+        const updataDogUrl = data.message;
+        setDogUrl(updataDogUrl)
+      }
+    },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  };
+
   return (
     <div>
       <header>React_Station2</header>
       <p>犬の画像です</p>
       <img src={dogUrl} />
 
-      <button onClick ={() => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")} />
+      <button onClick ={() => setDogUrl(updataImage())} />
     </div>
   )
 }
