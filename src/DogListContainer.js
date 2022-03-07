87848c621ce422983850834e1b1dc04007a2290e
kamioka24 <keee2404@gmail.com>
2022-03-07 14:45:55 +0900

    "コメントアウト追加"

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 
 export function DogListContainer() {
-  const [breeds, setBreeds] = useState("xxx");
+  const [breeds, setBreeds] = useState("xxx"); // 初期値ないなら"xxx"も必要なし？
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/list/all")
       .then(res => res.json())
